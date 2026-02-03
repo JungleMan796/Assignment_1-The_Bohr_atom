214f993335aee5b6312bc88da6316e030e9fb329
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-02-03 20:45:43 +0000

    repeating implementation, pt 1

--- ass1_test.cpp
+++ ass1_test.cpp
@@ -10,34 +10,39 @@ int main()
     // Define constants.
     const double R = 13.6; // Rydberg constant in eV.
     const double eV_to_J = 1.602e-19; // Conversion factor from eV to Joules.
-    
-    // Requesting variables from user. Will need to implement checks for invalid inputs later.
-    int units, Z, n_i, n_j; // Units calculation is printed in, atomic number, initial and final principal quantum numbers.
 
-    std::cout << "Enter atomic number Z: ";
-    std::cin >> Z;
-    std::cout << "Enter initial quantum number n_i: ";
-    std::cin >> n_i;
-    std::cout << "Enter final quantum number n_j: ";
-    std::cin >> n_j;
-    std::cout << "Enter units (0 for eV, 1 for Joules): ";
-    std::cin >> units;
-    
-    // Compute transition energy using Bohr formula.
-    double E = R * Z * Z * ((1 / (n_j * n_j)) - (1 / (n_i * n_i))); // Transition energy in eV.
+    // Define character for repetition of program.
+    char repeat = 'y';
+    do
+    {
+        // Requesting variables from user. Will need to implement checks for invalid inputs later.
+        int units, Z, n_i, n_j; // Units calculation is printed in, atomic number, initial and final principal quantum numbers.
 
-    // Convert to Joules if required and print result.
-    // Look into asking for precision, need joules in standard form most likely.
-    if (units == 1)
-    {
-        E = E * eV_to_J; // Convert eV to Joules, term is from UoM physical constants sheet.
-        std::cout << std::fixed << std::setprecision(20);
-        std::cout << "Transition energy: " << E << " J" << std::endl;
-    }
-    else
-    {
-        std::cout << std::fixed << std::setprecision(6);
-        std::cout << "Transition energy: " << E << " eV" << std::endl;
-    }
+        std::cout << "Enter atomic number Z: ";
+        std::cin >> Z;
+        std::cout << "Enter initial quantum number n_i: ";
+        std::cin >> n_i;
+        std::cout << "Enter final quantum number n_j: ";
+        std::cin >> n_j;
+        std::cout << "Enter units (0 for eV, 1 for Joules): ";
+        std::cin >> units;
+        
+        // Compute transition energy using Bohr formula.
+        double E = R * Z * Z * ((1 / (n_j * n_j)) - (1 / (n_i * n_i))); // Transition energy in eV.
+
+        // Convert to Joules if required and print result.
+        // Look into asking for precision, need joules in standard form most likely.
+        if (units == 1)
+        {
+            E = E * eV_to_J; // Convert eV to Joules, term is from UoM physical constants sheet.
+            std::cout << std::fixed << std::setprecision(20);
+            std::cout << "Transition energy: " << E << " J" << std::endl;
+        }
+        else
+        {
+            std::cout << std::fixed << std::setprecision(6);
+            std::cout << "Transition energy: " << E << " eV" << std::endl;
+        }
+    } while (repeat == 'y');
     
 }
